db32184e4706beb6ddef76c4ffaaa7cba43d46ec
Vikash Kumar <128958717+vikashKumar6941007@users.noreply.github.com>
2026-07-11 23:19:50 +0530

    Time: 0 ms (100%), Space: 35.7 MB (56.02%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0;
+
+        // Calculate total sum of vector
+        for (int num : nums) {
+            total += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            //leftSum + rightSum gives total sum of array
+            int rightSum = total - leftSum - nums[i];
+            //condition satisfied
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
