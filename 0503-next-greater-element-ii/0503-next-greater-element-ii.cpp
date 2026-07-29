cda1036eccb55b8e77512b4fef7b1c625f79e1a0
Vikash Kumar <128958717+vikashKumar6941007@users.noreply.github.com>
2026-07-30 00:21:14 +0530

    Time: 2 ms (78.85%), Space: 28.6 MB (40.14%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -10,10 +10,10 @@ public:
         // Monotonic decreasing stack (stores values)
         stack<int> st;
 
-        // Traverse twice from right to left
+        // Traverse twice from right to left to make the array double without actually doubling it
         for (int i = 2 * n - 1; i >= 0; i--) {
 
-            // Current index after wrapping around
+            // Current index after wrapping around, we double the value of n for i, so it should not get out of bound from the vector from which we are searching
             int idx = i % n;
 
             // Remove all smaller or equal elements
@@ -21,8 +21,8 @@ public:
                 st.pop();
             }
 
-            // Only fill answers during the first pass
-            // (when idx belongs to original array)
+            // Only fill answers during i < n , bcz i actually we are using this 2*n to fill the stack with the vector's elements and then start our next greater stack algo to really get the answer by modify ans vector
+            // (when idx belongs to original vector)
             if (i < n) {
 
                 if (!st.empty())
@@ -31,7 +31,7 @@ public:
                     ans[idx] = -1;
             }
 
-            // Push current element for future comparisons
+            // Push current element for future comparisons (filling the stack for later use when original indices arrives)
             st.push(nums[idx]);
         }
 
